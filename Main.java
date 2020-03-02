a124bd96a544766e3cc746412f8205f4f4718d80
Jayllian Marquardt <jayllian.marquardt.2021@student.lhschools.org>
2020-03-02 13:45:44 -0500

    intellij commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("My name is ");
+        System.out.print("Jayllian. ");
+        System.out.println("Over break i played basketball with my friends. ");
+        System.out.print("We ate a lot of food and played videogames after. ");
+        System.out.println("I also went to Myrtle Beach, it was pretty cool.")
+        //Print three more lines about what you did over break
+    }
+
+}
